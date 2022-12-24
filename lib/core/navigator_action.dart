2caf0d07e5d7767fd4654c52e155e95c0634d96c
cbmbbmj <cbmbbj@gmail.com>
2022-12-24 18:27:27 -0500

    comment

---
+++ lib/core/navigator_action.dart
@@ -0,0 +1,5 @@
+//import 'package:flutter/material.dart';
+
+// Widget defaultpushNamed(BuildContext ) {
+//  return Navigator.pushNamed(context, );
+// }
--- lib/view/home/home_screen.dart
+++ lib/view/home/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:isar_getx_todo_flutter/controller/counter_control.dart';
+import 'package:isar_getx_todo_flutter/widgets/default_show_model_bottom_sheet.dart';
 import 'package:provider/provider.dart';
 import 'package:step_progress_indicator/step_progress_indicator.dart';
 
@@ -25,37 +26,44 @@ class MyHomePage extends StatelessWidget {
               children: <Widget>[
                 CircularStepProgressIndicator(
                   totalSteps: 33,
-                  currentStep: context.watch<Counter>().count%33,
+                  currentStep: context.watch<Counter>().count % 33,
                   stepSize: 10,
                   selectedColor: context.watch<Counter>().selectedColor,
                   unselectedColor: Colors.grey.shade200,
-                  width: MediaQuery.of(context).size.width*0.7,
-                  height: MediaQuery.of(context).size.width*0.7,
-                  child:const Center(child: Count(), ),
+                  width: MediaQuery.of(context).size.width * 0.7,
+                  height: MediaQuery.of(context).size.width * 0.7,
+                  child: const Center(
+                    child: Count(),
+                  ),
                 ),
-               
               ],
             ),
           ),
         ),
       ),
-      floatingActionButton: context.watch<Counter>().count ==99?
-   
-           FloatingActionButton(
-           backgroundColor:  Colors.pink.shade600,
-            onPressed: () =>  context.read<Counter>().restartCounter(),
-            tooltip: 'restart counter',
-            child:  const Icon(Icons.restart_alt_rounded ),
- 
-      ): FloatingActionButton(
-        backgroundColor:context.watch<Counter>().selectedColor ,
-        key: const Key('increment_floatingActionButton'),
-        onPressed: () => context.read<Counter>().increment(),
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+      floatingActionButton: context.watch<Counter>().count == 1
+          ? FloatingActionButton(
+              backgroundColor: Colors.pink.shade600,
+              // onPressed: () =>  context.read<Counter>().restartCounter(),
+              onPressed: () {
+                //========== show Model BottomSheet ==========//
+                defaultShowModelBottomSheet(context: context );
+                //========== show Model BottomSheet ==========//
+              },
+              tooltip: 'restart counter',
+              child: const Icon(Icons.restart_alt_rounded),
+            )
+          : FloatingActionButton(
+              backgroundColor: context.watch<Counter>().selectedColor,
+              key: const Key('increment_floatingActionButton'),
+              onPressed: () => context.read<Counter>().increment(),
+              tooltip: 'Increment',
+              child: const Icon(Icons.add),
+            ),
     );
   }
+
+
 }
 
 class Count extends StatelessWidget {
@@ -66,7 +74,8 @@ class Count extends StatelessWidget {
     return Text(
       '${context.watch<Counter>().count}',
       key: const Key('counterState'),
-      style: TextStyle(color: context.watch<Counter>().selectedColor ,fontSize: 24.0),
+      style: TextStyle(
+          color: context.watch<Counter>().selectedColor, fontSize: 24.0),
     );
   }
 }
